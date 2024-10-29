aa3a5b7dc30693ff8423eae0855bd01db400aaa5
Tatyana Smirnova <smrts@mail.ru>
2024-10-29 10:17:32 +0500

    Четвёртое задание полностью готово. Доработка

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,32 +1,30 @@
 function Student(name, gender, age) {
-    this.name = name;
-    this.gender = gender;
-    this.age = age;
-    this.marks = [];
-  }
-  
-  Student.prototype.setSubject = function(subjectName) {
-    this.subject = subjectName
-  }
-  
-  Student.prototype.addMarks = function(...marks) {
-    if("marks" in this) {
-      this.marks.push(...marks);
-    }  
-  }
-  
-  Student.prototype.getAverage =  function() {
-    if (!("marks" in this)) {
-      return 0;
-    }    
-    if(this.marks.length === 0) {
-      return 0;
-    }
-    return +(this.marks.reduce( (accumulator, currentValue, index, arr) => accumulator + currentValue/arr.length, 0).toFixed(2));
-  }
-  
-  Student.prototype.exclude = function(reason) {
-   this.exclude = reason;
-   delete this.subject;
-   delete this.marks;
-  }
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
+}
+
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName
+}
+
+Student.prototype.addMarks = function(...marks) {
+  if("marks" in this) {
+    this.marks.push(...marks);
+  }  
+}
+
+Student.prototype.getAverage =  function() {
+  if (!("marks" in this) || this.marks.length === 0) {
+    return 0;
+  }    
+
+  return this.marks.reduce( (accumulator, currentValue, index, arr) => accumulator + currentValue/arr.length, 0);
+}
+
+Student.prototype.exclude = function(reason) {
+ Object.assign(this, {"exclude": reason})
+ delete this.subject;
+ delete this.marks;
+}
